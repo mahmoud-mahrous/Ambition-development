66e4638212384b4acef458a868b0b2a403e45ed5
mahm <mahmoudmahrous0707@gmail.com>
2022-12-06 07:22:12 -0800

    upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('.header-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:false
+  });
+
+
+
+
+
+  $('.reviews-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows:false,
+    dots:true
+  });
+
+
+
+  
+ 
